e1dd4adea89073d0514372a3be03f78a01abad3e
Ghostday <fablekey@gmail.com>
2022-03-13 02:44:04 +1100

    Started work on landing page

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,3 +1,15 @@
+import { useState, useEffect } from 'react'
+import Login from '../components/Login'
+import Signup from '../components/Signup'
+
 export default function LandingPage() {
-    
+
+    const [auth, setAuth] = useState(null)
+
+
+    return (
+        <div>
+        { auth ? <Login /> : <Signup />}    
+        </div>
+    )
 }
