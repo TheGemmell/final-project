9a4b1626c2aa1ce2a40429c387d2b95f0871ed9d
Ghostday <fablekey@gmail.com>
2022-03-17 17:02:12 +1100

    Ensured at least one user is seeded

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(username: 'root', password_digest: 'test')
+user1 = User.create(username: 'root', password_digest: 'test', firstname: 'chris', lastname: 'gemmell')
+user1.save
