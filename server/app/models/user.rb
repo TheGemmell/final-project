e5e870201f4ae891aac9594c1eb068e82513721f
Ghostday <fablekey@gmail.com>
2022-03-17 20:21:38 +1100

    added some Requiremenets to help further issues be bugfixed

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,4 +1,8 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+    # Automatically adds methods to check hashed password
     has_secure_password
+
     validates :username, uniqueness: { case_sensitive: false }
 end
