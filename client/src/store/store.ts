d17c5a355108f968aa5c88697b788d04376b2ad1
Chris Gemmell <fablekey@gmail.com>
2022-03-29 00:44:19 +1100

    Now using configureStore instead of createStore for easier abstraction

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,21 +1,20 @@
 import { createStore, applyMiddleware, Reducer, Action } from 'redux'
-import reducers from './reducers/index'
-import thunk from 'redux-thunk'
-import { DefaultRootState } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './reducers/userReducer';
 
 export type GlobalState = { 
     user: string;
 }
 
-const store = createStore<DefaultRootState, Action, any, any>(
-    //Reducers Go Here
-    reducers,
-
-    //Initial State
-    {},
-
-    //Middleware 
-    applyMiddleware(thunk)
-)
+const store = configureStore({
+    reducer: {
+        user: userReducer,
+    },
+})
 
 export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
