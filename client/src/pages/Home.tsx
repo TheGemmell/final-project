8645172eaa4afcb81ce65a26f19ac5483b325f45
Chris Gemmell <fablekey@gmail.com>
2022-04-05 13:47:17 +1000

    Added Home Page to be directed to after Auth

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,15 +1,33 @@
+import { Box, Grid, Paper, styled } from '@mui/material'
 import { useState, useEffect } from 'react'
 import Login from './Login'
 import Signup from './Signup'
 
 export default function Home() {
 
-    const [auth, setAuth] = useState(null)
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Item>
+          <h1>Workout App</h1>
+        </Item>
+      </Grid>
+      <Grid item xs={12}>
+        <Item>
+          App
+        </Item>
+      </Grid>
 
 
-    return (
-        <div>
-        { auth ? <Login /> : <Signup />}    
-        </div>
-    )
+
+    </Grid>
+  )
 }
