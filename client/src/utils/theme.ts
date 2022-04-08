694e25280cffda9bd33a24c1e85b16baccc69600
Ghostday <fablekey@gmail.com>
2022-04-08 22:31:53 +1000

    Added palette theming for MUI

---
+++ client/src/utils/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material';
+import { purple, blue, red } from '@mui/material/colors';
+
+const theme = createTheme({
+    palette: {
+        // mode: "dark",
+        primary: {
+            main: '#062d56',
+        },
+        secondary: {
+            main: '#11cb5f',
+        },
+    },
+})
+
+export default theme
