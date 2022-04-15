0c1ea1ab3ae14099d11d2b118002af1b8f2df428
Ghostday <fablekey@gmail.com>
2022-04-15 10:51:14 +1000

    continued

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,8 @@
 import { Box, Grid, Paper, styled } from '@mui/material'
+import Item from '../components/Item'
 import { useState, useEffect } from 'react'
-import Login from './Login'
-import Signup from './Signup'
 
 export default function Home() {
-
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#f3e5f5',
-    ...theme.typography.body2,
-    padding: theme.spacing(0.2),
-    textAlign: 'center',
-    color: theme.palette.text.primary,
-  }));
 
   return (
     <Grid container spacing={2}>
